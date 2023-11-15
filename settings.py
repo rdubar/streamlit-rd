ec05c67b307fb41e9145a86f895267eb16cdae20
RogerDubar <rdubar@gmail.com>
2023-11-15 21:23:56 +0000

    Requirement & path updates

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 import os
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
-DATA_DIR = ROOT_DIR + '/data/'
-MEDIA_RECORDS_PATH = DATA_DIR + 'media_records.data'
-DATAFRAME_PATH = DATA_DIR + 'plex_df.pkl'
-LIBRARY_PATH = DATA_DIR + 'library.txt'
-FILE_OBJECTS_PATH = DATA_DIR + 'file_objects.pkl'
-TOML_PATH = ROOT_DIR + '/.streamlit/secrets.toml'
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # Project Root
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+MEDIA_RECORDS_PATH = os.path.join(DATA_DIR, 'media_records.data')
+DATAFRAME_PATH = os.path.join(DATA_DIR, 'plex_df.pkl')
+LIBRARY_PATH = os.path.join(DATA_DIR, 'library.txt')
+FILE_OBJECTS_PATH = os.path.join(DATA_DIR, 'file_objects.pkl')
+TOML_PATH = os.path.join(ROOT_DIR, '.streamlit/secrets.toml')
 IGNORE_LIST = ['/Incoming/']
 
